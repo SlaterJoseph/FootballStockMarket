a6ab0ecdddb1803fb9e1e384ade3580257c6ed0c
SlaterJoseph <Josephslater03@gmail.com>
2024-05-06 15:28:48 -0400

    Initial Commit

---
+++ src/main/java/com/project/footballstockmarket/FootballStockMarketApplication.java
@@ -0,0 +1,13 @@
+package com.project.footballstockmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballStockMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballStockMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/footballstockmarket/FootballStockMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.footballstockmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballStockMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

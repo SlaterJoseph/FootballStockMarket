1d0f57d0d2009c324ab7bda7783a3359ac05245a
SlaterJoseph <Josephslater03@gmail.com>
2024-05-14 15:40:55 -0400

    Finished revising weekly stat collection

--- src/main/python/Model/Collection/weekly_stats_collection.py
+++ src/main/python/Model/Collection/weekly_stats_collection.py
@@ -1,9 +1,28 @@
 import csv
+import glob
+import os
 import requests
 import json
 import datetime
 
 year = datetime.date.today().year
+
+positional_weekly_stats = {
+    'LB': {'defensive', 'defensiveInterceptions', 'general', 'scoring', 'returning'},
+    'RB': {'rushing', 'returning', 'receiving', 'passing', 'scoring', 'general'},
+    'WR': {'rushing', 'returning', 'receiving', 'passing', 'scoring', 'general'},
+    'TE': {'rushing', 'returning', 'receiving', 'passing', 'scoring', 'general'},
+    'DT': {'defensive', 'defensiveInterceptions', 'general', 'scoring', 'returning'},
+    'DE': {'defensive', 'defensiveInterceptions', 'general', 'scoring', 'returning'},
+    'CB': {'defensive', 'defensiveInterceptions', 'general', 'scoring', 'returning'},
+    'P': {'punting', 'kicking', 'general', 'passing', 'rushing', 'returning'},
+    'PK': {'kicking', 'punting', ' general', 'passing', 'rushing', 'returning'},
+    'QB': {'rushing', 'returning', 'receiving', 'passing', 'scoring', 'general', 'returning'},
+    'S': {'defensive', 'defensiveInterceptions', 'general', 'scoring', 'returning'},
+    'FB': {'rushing', 'returning', 'receiving', 'passing', 'scoring', 'general', 'returning'},
+    'DL': {'defensive', 'defensiveInterceptions', 'general', 'scoring', 'returning'},
+    'DB': {'defensive', 'defensiveInterceptions', 'general', 'scoring', 'returning'},
+}
 
 
 def get_player_stats() -> None:
@@ -27,9 +46,14 @@ def get_player_stats() -> None:
     # Iterating over players grabbing their stats over the 3 previous seasons
     for player in player_list:
         print(player[0])
+        position = player[5]
 
         espn_id = player[11]
         for season in seasons:
+            # Skip over offensive line positions
+            if position not in positional_weekly_stats.keys():
+                continue
+
             url = f'https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/' \
                   f'seasons/{season}/athletes/{espn_id}/eventlog'
             response = json.loads(requests.get(url).text)
@@ -41,13 +65,20 @@ def get_player_stats() -> None:
                         stats = json.loads(requests.get(stats_url).text)
 
                         for entry in stats['splits']['categories']:
-                            cat = entry['name']
+                            # Weed out the stats which don't affect certain players
+                            if entry['name'] not in positional_weekly_stats[position]:
+                                continue
+
+                            group = entry['name']
                             performance = list()
+                            performance.append(player[0])
+                            performance.append(player[11])
+                            performance.append(season)
 
                             for stat in entry['stats']:
                                 performance.append(stat['value'])
 
-                            csvs[cat].writerow(performance)
+                            csvs[group].writerow(performance)
                     # Did not play a game that season
                     else:
                         continue
@@ -68,7 +99,7 @@ def build_csv_headers() -> {str: list}:
     for entry in response['splits']['categories']:
         title = entry['name']
         stats = [chunk['abbreviation'] for chunk in entry['stats']]
-
+        stats = ['name', 'espnId', 'season'] + stats
         csv_headers[title] = stats
 
     return csv_headers
@@ -79,6 +110,16 @@ def initiate_csvs() -> {str: csv.writer}:
     Initializes the csv writers for the different csvs
     :return: A dictionary of str : writers, where the key is the name and the value is the csv writer
     """
+
+    # Deleting the old CSVs
+    try:
+        files = glob.glob(os.path.join('../..//CSVs/Weekly', '*'))
+        for file in files:
+            if os.path.isfile(file):
+                os.remove(file)
+    except OSError:
+        print("Error occurred while deleting files.")
+
     csvs_titles = ['general', 'passing', 'rushing', 'receiving', 'defensive', 'defensiveInterceptions', 'kicking',
                    'returning', 'punting', 'scoring']
 
@@ -86,7 +127,7 @@ def initiate_csvs() -> {str: csv.writer}:
     csvs = dict()
 
     for title in csvs_titles:
-        writer = csv.writer(open(f'../../CSVs/{title}.csv', 'a', newline=''))
+        writer = csv.writer(open(f'../../CSVs/Weekly/{title}.csv', 'a', newline=''))
         writer.writerow(csv_headers[title])
         csvs[title] = writer
 
@@ -94,17 +135,3 @@ def initiate_csvs() -> {str: csv.writer}:
 
 
 get_player_stats()
-
-"""
-Different CSVs
-General
-Passing
-Rushing
-Receiving
-Defensive
-Defensive Interceptions
-Kicking
-Returning
-Punting
-Scoring
-"""
